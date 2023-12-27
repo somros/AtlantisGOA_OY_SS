668a11f38e4c4a098aae404cff57b43c349f8a1f
somros <alberto.rovellini@gmail.com>
2023-12-27 14:21:12 -0800

    uploading files

---
+++ RunAtlantis_124.sh
@@ -0,0 +1,1 @@
+atlantisMerged -i GOA_cb_summer.nc  0 -o output_124.nc -r GOA_run.prm -f GOA_force_base.prm -p GOA_physics.prm -b GOAbioparam_test.prm -h GOA_harvest_124.prm -m GOAMigrations.csv -s GOA_Groups.csv -q GOA_fisheries.csv -d output
---
+++ RunAtlantis_129.sh
@@ -0,0 +1,1 @@
+atlantisMerged -i GOA_cb_summer.nc  0 -o output_129.nc -r GOA_run.prm -f GOA_force_base.prm -p GOA_physics.prm -b GOAbioparam_test.prm -h GOA_harvest_129.prm -m GOAMigrations.csv -s GOA_Groups.csv -q GOA_fisheries.csv -d output
